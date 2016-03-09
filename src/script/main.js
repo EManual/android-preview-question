81b6d893a7e26fb0ab57a8a6790e14ece4682ef2
Jayin Ton <tonjayin@gmail.com>
2016-03-09 13:16:02 +0800

    update

--- src/script/main.js
+++ src/script/main.js
@@ -1,20 +1,23 @@
 (function(window,$){
   $(document).ready(function(){
-    var selector_title = $('.weui_article h1')
+    var selector_description = $('#question_description')
     var selector_option_container = $('#option-container')
     var select_tpl_option = $('#tpl_option')
+    var select_question_answer = $('#question_answer')
     /**
      * 渲染题目
      */
     window.updateQuestion = function(question){
        
-       selector_title.html(question.description)
+       selector_description.html(question.description)
        if(question.type === 'choice'){
          var options = []
          question.options.forEach(function(option, index){
            options.push(select_tpl_option.html().replace(/{{option_id}}/g, 'option_'+index).replace(/{{option_description}}/g, option))
          })
          selector_option_container.html(options.join(''))
+         question.answer = question.answer || '答案: 无'
+         select_question_answer.html(question.answer)
        }
     }
     /**
@@ -29,7 +32,5 @@
       })
       return checkeds
     }
-    
-    console.log(window.Bridge)
   })  
 })(window, Zepto);
