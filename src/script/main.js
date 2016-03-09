1a6578d42798dd39f76b2559c0dcc2479685a173
Jayin Ton <tonjayin@gmail.com>
2016-03-09 13:21:27 +0800

    update

--- src/script/main.js
+++ src/script/main.js
@@ -1,5 +1,5 @@
-(function(window,$){
-  $(document).ready(function(){
+(function(window, $) {
+  $(document).ready(function() {
     var selector_description = $('#question_description')
     var selector_option_container = $('#option-container')
     var select_tpl_option = $('#tpl_option')
@@ -7,30 +7,32 @@
     /**
      * 渲染题目
      */
-    window.updateQuestion = function(question){
-       
-       selector_description.html(question.description)
-       if(question.type === 'choice'){
-         var options = []
-         question.options.forEach(function(option, index){
-           options.push(select_tpl_option.html().replace(/{{option_id}}/g, 'option_'+index).replace(/{{option_description}}/g, option))
-         })
-         selector_option_container.html(options.join(''))
-         question.answer = question.answer || '答案: 无'
-         select_question_answer.html(question.answer)
-       }
+    window.updateQuestion = function(question) {
+      //渲染描述
+      selector_description.html(question.description)
+
+      if (question.type === 'choice') {
+        var options = []
+        question.options.forEach(function(option, index) {
+          options.push(select_tpl_option.html().replace(/{{option_id}}/g, 'option_' + index).replace(/{{option_description}}/g, option))
+        })
+        selector_option_container.html(options.join(''))
+      }
+      //渲染答案
+      question.answer = question.answer || '答案: 无'
+      select_question_answer.html(question.answer)
     }
     /**
      * @return array 已选择的东西列表
      */
-    window.getUserAnswer = function(){
+    window.getUserAnswer = function() {
       var checkeds = []
-      $('#option-container input').forEach(function(input, index){
-        if($(input)[0].checked){
+      $('#option-container input').forEach(function(input, index) {
+        if ($(input)[0].checked) {
           checkeds.push(index)
         }
       })
       return checkeds
     }
-  })  
+  })
 })(window, Zepto);
